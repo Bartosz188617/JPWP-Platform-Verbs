36e28ae0592b387c1cf1efcec53ef954370eb365
Bartosz188617 <s188617@student.pg.edu.pl>
2024-01-04 14:42:36 +0100

    Words disposal is working with a timer.

--- Form1.cs
+++ Form1.cs
@@ -1,22 +1,79 @@
 using System.Diagnostics;
 using System.Numerics;
 using System.Reflection.Emit;
+using System.Text;
 using System.Windows.Forms;
 
 namespace JPWP_Platform_Verbs
 {
     public partial class PlatformVerbsForm : Form
     {
-        private int counter = 0, timeSec = 15;
-        private int score = 1;
-        private bool right, left, jump, jumpON, on_platform;
+        private string[] correct_verbs = new string[]
+        {
+            "01/BRING",
+            "02/SEND",
+            "03/SEE",
+            "04/GO",
+            "05/TAKE",
+            "06/GET",
+            "07/MAKE",
+            "08/KNOW",
+            "09/THINK",
+            "10/SPEAK",
+            "11/COME",
+            "12/FIND",
+            "13/GIVE",
+            "14/TELL",
+            "15/SEEK",
+            "16/KEEP",
+            "17/PUT",
+            "18/MAY",
+            "19/HOLD",
+            "20/LEAVE",
+            "21/HEAR",
+            "22/STAND",
+            "23/FALL",
+            "24/LEARN",
+            "25/MEAN",
+            "26/BUILD",
+            "27/BURN",
+            "28/BUY",
+            "29/TEACH",
+            "30/FIGHT",
+            "31/THINK",
+            "32/LEND",
+            "33/LOSE",
+            "34/MISTAKE",
+            "35/WIN",
+            "36/BREAK",
+            "37/CHOOSE",
+            "38/DRINK",
+            "39/FORGET",
+            "40/FORGIVE",
+            "41/SLEEP",
+            "42/SWIM",
+            "43/RIDE",
+            "44/RUN",
+            "45/EAT",
+            "46/WEAR",
+            "47/DRIVE",
+            "48/RING",
+            "49/RISE",
+            "50/WRITE"
+        };
+
+        Random rnd = new Random();
+
+        private int[] used = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        private int counter = 0, timeSec = 16, index;
+        private bool right, left, jump, on_platform;
         private int gravity = 20;
         private int force;
-        String line, verb;
+
 
         Font LargeFont = new Font("Palatino Linotype", 20);
 
-        private string platform_number;
+        private string platform_number, verb;
 
 
         public PlatformVerbsForm()
@@ -28,6 +85,10 @@ namespace JPWP_Platform_Verbs
             TheWord.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             TheWord.Size = new Size(TheWord.PreferredWidth, TheWord.PreferredHeight);
             TheWord.Font = LargeFont;
+
+            index = rnd.Next(0, 49);
+            used[0] = index;
+
         }
 
         private void gameKeyDown(object sender, KeyEventArgs e)
@@ -133,7 +194,12 @@ namespace JPWP_Platform_Verbs
                 (Player.Location.X > 715 && Player.Location.X < 813) || 
                 (Player.Location.X > 1013 && Player.Location.X < 1230))
             {
-                on_platform = false;
+                if (on_platform == true) 
+                { 
+                    on_platform = false; 
+                    platform_number = "none"; 
+                    System.Diagnostics.Debug.WriteLine(platform_number);
+                }
             }
 
             if (!on_platform)
@@ -144,25 +210,21 @@ namespace JPWP_Platform_Verbs
 
             //displaying verbs
             #region
-            try
-            {
-                StreamReader verbs = new StreamReader(Application.StartupPath + "\\VERBS\\VERBS_CORRECT.txt");
-                line = verbs.ReadLine();
-
-                int check = timeSec;
-
-                while (line != null)
+            if (timeSec == 0)
+            {               
+                verb = correct_verbs[index];
+                TheWord.Text = verb.Remove(0, 3);
+                timeSec = 15;
+                index = rnd.Next(0, 49);
+                for(int k = 0; k < 49; k++) //checking if generated number is new
                 {
-                    verb = line;
-                    TheWord.Text = verb.Remove(0, 3);
-                    line = verbs.ReadLine();
+                    if(used[k] == index)
+                    {
+                        index = rnd.Next(0, 49);
+                        k = 0;
+                    }
                 }
-
-                verbs.Close();
-            }
-            catch
-            {
-                System.Diagnostics.Debug.WriteLine("file error");
+                used[index] = index;
             }
             #endregion
 
@@ -182,7 +244,7 @@ namespace JPWP_Platform_Verbs
                     {
                         Timer.Text = timeSec.ToString();
                     }
-                    //System.Diagnostics.Debug.WriteLine(Timer.Text);
+                    //System.Diagnostics.Debug.WriteLine(timeSec);
                     counter = 0;
                 }
                 counter++;
